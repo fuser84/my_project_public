4dc37468903a47c4cb996c889d52981619670d13
Volodymyr Novodid <vnovodid@gmail.com>
2017-10-03 00:20:47 +0300

    my_project

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+
+    $("#loginModalClick1").click(function () {
+        $('#loginModal1').modal('show');
+    });
+    $("#loginModalClick2").click(function () {
+        $('#loginModal2').modal('show');
+    });
+});
+
+function myMap() {
+    var mapCanvas = document.getElementById("map");
+    var mapOptions = {
+        center: new google.maps.LatLng(51.5, -0.2), zoom: 10
+    };
+    var map = new google.maps.Map(mapCanvas, mapOptions);
+}
